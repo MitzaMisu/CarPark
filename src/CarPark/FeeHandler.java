3e2de8b72090d4da466f613d5ed97a5cdebe521e
Mihai Fleser <mihaifleser@yahoo.com>
2021-01-12 21:22:53 +0200

    Update

--- src/CarPark/FeeHandler.java
+++ src/CarPark/FeeHandler.java
@@ -9,6 +9,10 @@ public class FeeHandler {
     private final Integer bigFee = 3;
     private final Integer mediumFee = 2;
     private final Integer smallFee = 1;
+
+    private final Integer bigFeeDiff = 60;
+    private final Integer mediumFeeDiff = 180;
+
     public FeeHandler(Calendar currentDate)
     {
         this.currentDate = currentDate;
@@ -22,11 +26,11 @@ public class FeeHandler {
         long diffInMillies = c.getExitTicket().getTicketDate().getTime() - c.getEntryTicket().getTicketDate().getTime();
         long diff = TimeUnit.MINUTES.convert(diffInMillies, TimeUnit.MILLISECONDS);
         //System.out.println("AAAAAAAAAAAAAAAAaaa" + diffInMillies);
-        if(diff < 20)
+        if(diff < bigFeeDiff)
             payFee = bigFee;
-        if(diff < 40)
+        if(diff < mediumFeeDiff && diff > bigFeeDiff)
             payFee = mediumFee;
-        if(diff < 60)
+        if(diff > mediumFeeDiff)
             payFee = smallFee;
         long fee = payFee * diff;
         totalFeesColected += fee;
